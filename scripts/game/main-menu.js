77e235aa50214b8e003bfe5ef15be2d8b76ee2ed
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-08 15:47:55 -0400

    Update main-menu.js

--- scripts/game/main-menu.js
+++ scripts/game/main-menu.js
@@ -10,3 +10,24 @@ function showMainMenu(){
     ctx.font = "100px Sniglet";
     ctx.fillText("Paused", 880, 525);
 }
+
+function mainMenuHandler(){
+    switch(event.keyCode){        
+        case 13:
+            mainMenuOn = false;
+            document.onkeydown = null;
+            document.onkeydown = levelKeyDownHandler;
+            break;
+        case 38:
+            alert("up");
+            break;
+        case 40:
+            alert("down");
+            break;
+        case 70:
+            toggleFullScreen();
+            break;
+        default:
+            break;
+    }
+}
