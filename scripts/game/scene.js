ae9b3d8d8d35d9d7b202e87e22a7af06e006d600
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-09 16:13:55 -0400

    Update scene.js

--- scripts/game/scene.js
+++ scripts/game/scene.js
@@ -342,7 +342,7 @@ function saveFileHandler(){
                 cancelAnimationFrame(drawing);    
                 showStartMenu();
             }else{
-                alert("Save option " + (currentOption + 1) + " selected.");
+                alert("Save file " + (currentOption + 1) + " selected.");
             }
             break;
         case 38:
