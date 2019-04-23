8be490210104b911edd80cc6af8eeffded46d35d
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-23 12:49:39 -0400

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -23,21 +23,30 @@ function createUser(){
     //gets the values entered in the form for the new user
     var username = document.getElementById("usernameNew").value;
     var password = document.getElementById("passwordNew").value;
-    
-    //Firebase authorization requires a password to login,
-    //so automatically adds a fake email account suffix to every username
-    username += "@dummy.com";
-    
-    firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
-        //if an error occured, displays the error message to the user
-        errorMessage.innerHTML = error.message;
-        var formFields = document.getElementsByTagName("input");
-        for(var i = 0; i < formFields.length; i++){
-                formFields[i].className += " form-error";
-        }
-        
+    var displayName = document.getElementById("displayNameNew").value;
+
+    if(!displayName){
+        errorMessage.innerHTML = "Display name cannot be blank.";
+    }else{
+        //displays loading message while attempting to connect to the Firebase database
+        var waiting = document.getElementById("waiting");
+        waiting.innerHTML = "Waiting...";
+
+        //Firebase authorization requires a password to login,
+        //so automatically adds a fake email account suffix to every username
+        username += "@dummy.com";
+
+        firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
+            //if an error occured, displays the error message to the user
+            errorMessage.innerHTML = error.message;
+            var formFields = document.getElementsByTagName("input");
+            for(var i = 0; i < formFields.length; i++){
+                    formFields[i].className += " form-error";
+            }
+            
         waiting.innerHTML = "";
-    });
+        });
+    }
 
 }
 
