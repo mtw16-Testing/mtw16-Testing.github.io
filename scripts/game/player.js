399b921aee6ba293acb40adf5b9bdb6fce557b56
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-24 17:22:35 -0400

    Update player.js

--- scripts/game/player.js
+++ scripts/game/player.js
@@ -33,7 +33,7 @@ function initPlayer(options) {
 	that.draw = function() {
 		ctx.drawImage(that.image,63*that.aFrame,63*(action+that.direction),63,63,that.X,that.Y,126,126);
 		//
-		ctx.strokeRect(63*that.aFrame, 63*(action+that.direction), 63, 63);
+		ctx.strokeRect(that.X,that.Y,126,126);
 		//
 		ctx.fillRect(that.iBox[0],that.iBox[2],30,31);
 		drawHealth(that);
