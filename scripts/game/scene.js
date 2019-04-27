a2038ce41b17517cd20179bc883abf17132af99d
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-27 12:39:30 -0400

    Update scene.js

--- scripts/game/scene.js
+++ scripts/game/scene.js
@@ -139,7 +139,7 @@ function SceneHandler(scene){
 		   	tile.empty = true;   
 		   }
 		       
-		   if(pixelData[row+j+1] == 0 && pixelData[row+j+2] == 76){
+		   if(pixelData[row+j+1] == 0 && pixelData[row+j+2] == 176){
 			//bounds.push(tile);		   	   
 			var newVillager = new initVillager({
 					X: (j/4)*64,
