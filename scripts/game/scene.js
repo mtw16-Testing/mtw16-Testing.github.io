9e2f8e24cf4107c97cb08aed60efaba75a794a91
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-27 10:35:08 -0400

    Update scene.js

--- scripts/game/scene.js
+++ scripts/game/scene.js
@@ -11,6 +11,12 @@ Y: 1000,
 sentence: "My Lord! The prophecies heralded your return. Your path to take back your throne begins now, sire. Your rivals stand in your way, once you defeat them, you may leave this province in the top right and head towards the castle!"
 }));
 
+Villagers.push(new initVillager({
+X: 500,
+Y: 800,
+sentence: "Please work"
+}));
+
 var Enemies = new Array();
 Enemies.push(new initEnemy({ 
 	X: 500,
