64f6add1e0936563847dfb02ff3b87121218ed3c
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-28 16:20:58 -0400

    Update levels.js

--- scripts/game/levels.js
+++ scripts/game/levels.js
@@ -206,9 +206,9 @@ function loadBeach(side){
 	sceneHandler.scene.nextMaps[0] = "Level 1";
 	
 	Villagers.push(new initVillager({
-			X: 1400,
-			Y: 1300,
-			sentence: "Long have we awaited your return!",
+			X: 1600,
+			Y: 1100,
+			sentence: "Sorry, all of my boats are rented out. I can't give you one right now, come back later.",
 			type: "interact"
 			}));
 
