e24b705eea692c664d4e79ea93ef12be932b8b10
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-28 13:03:47 -0400

    Update init.js

--- scripts/game/init.js
+++ scripts/game/init.js
@@ -52,6 +52,6 @@ var pLeft = false, pRight = false, pDown = false, pUp = false;
 var isBlocked = false;
 var sideOfScreen = -1;
 
-var interactionMenuImage = new Image();
+var interactionMenuImage;
 
 mapEntries = ["Level 1"];
