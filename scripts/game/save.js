819cb97f5b858352ad99e4c74e17ce8532c14d57
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-28 10:02:44 -0400

    Update save.js

--- scripts/game/save.js
+++ scripts/game/save.js
@@ -1,3 +1,5 @@
+var startTime = 0;
+
 //Save File information
 function SaveFile(data){
 	this.name = data.name,
@@ -6,3 +8,26 @@ function SaveFile(data){
 }
 
 var saveFiles = new Array();
+
+function saveGame(){
+	var oldTime;
+	var endTime = new Date();
+	var timeElapsed = (endTime.getTime() - startTime.getTime())/1000;
+	
+	startTime = endTime;
+	
+	var timeMinutes = Math.floor(timeElapsed / 60);
+	var timeSeconds = timeElapsed % 60;
+	
+	db.collection('SaveFile').doc(user.uid).get().then(doc=> {
+		oldTime = doc.data().time;
+            }).catch(function(error) {
+                alert("Unknown error, unable to save.");
+            });
+	
+	db.collection('SaveFile').doc(firebase.auth().currentUser.uid).set({
+                minutes: timeMinutes,
+		seconds: timeSeconds,
+		location: sceneHandler.scene.name
+        });
+}
