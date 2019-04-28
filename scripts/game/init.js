a16fd796347423672153ba2f4610adc975eb8749
mtw16-Testing <49355201+mtw16-Testing@users.noreply.github.com>
2019-04-28 14:03:10 -0400

    Update init.js

--- scripts/game/init.js
+++ scripts/game/init.js
@@ -40,6 +40,7 @@ var endTiles = new Array();
 //detects if all images have been loaded in before starting the level
 var isImage1Loaded = false;
 var isImage2Loaded = false;
+var isImage3Loaded = false;
 var isSpreadsheetLoaded = false;
 
 var image1;
